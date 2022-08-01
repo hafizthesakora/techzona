d6de1cbf3e9658b7fbc68e3ac955de4b2b09bae8
hafizthesakora <64905522+hafizthesakora@users.noreply.github.com>
2022-07-31 17:42:53 -0700

    a step further

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -47,7 +47,7 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>Amazona</title>
+        <title>Tekstore</title>
       </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
